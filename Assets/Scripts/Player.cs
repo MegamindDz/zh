ae21359e640c49ea26a0b7d8c8a0f55186c32f2e
MegamindDz <kitsada.th.64@ubu.ac.th>
2022-08-23 15:27:36 +0700

    Again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -9,12 +10,17 @@ public class Player : MonoBehaviour
     AudioSource asource;
     float hp = 100;
     Image imgHP;
+    public GameObject text1;
+    public GameObject button1;
+    Zombie MyZombie;
+
     
     // Start is called before the first frame update
     void Start()
     {
         asource = GetComponent<AudioSource>();
         imgHP =  GameObject.Find("HP").GetComponent<Image>();
+        
     }
 
     // Update is called once per frame
@@ -23,17 +29,41 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             asource.PlayOneShot(firearm);
+            Ray ray;
+            RaycastHit hit;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("Zombie"))
+                {
+                    MyZombie = hit.collider.gameObject.GetComponent<Zombie>();
+                    MyZombie.zombieHP -= 25;
+                    if(MyZombie.zombieHP <=0)
+                    {
+                        NavMeshAgent agent = MyZombie.GetComponent<NavMeshAgent>();
+                        Animator anim = MyZombie.GetComponent<Animator>();
+                        agent.speed = 0;
+                        anim.SetFloat("hp", 0);
+                        
+                    }    
+                }
+            }
         }
         imgHP.fillAmount = hp / 100;
+        if(hp <= 0)
+        {
+            text1.SetActive(true);
+            button1.SetActive(true);
+        }    
     }
 
-    void OnTriggerEnter(Collider other)
+    /*void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Zombie"))
         {
             hp -= 20;
         }
-    }
+    }*/
 
     void OnTriggerStay(Collider other)
     {
